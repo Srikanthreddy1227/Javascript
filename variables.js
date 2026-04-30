0570fa314bf4671a1595211af846d660cc05274f
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-04-30 14:19:25 +0530

    add variables.js files

---
+++ variables.js
@@ -0,0 +1,16 @@
+var name="srikanth";
+console.log(name);
+
+// var is used to declare a variable in JavaScript. It can be reassigned and redeclared within its scope. However, it is generally recommended to use let or const instead of var for better scoping and to avoid potential issues with hoisting.
+
+// In JavaScript, there are three ways to declare variables: var, let, and const. Each has its own characteristics and use cases.
+
+
+let age=25;
+console.log(age);
+
+// let is used to declare a block-scoped variable in JavaScript. It can be reassigned but cannot be redeclared within the same scope. It is generally preferred over var for better scoping and to avoid potential issues with hoisting.
+
+
+const  place="hyderabad";
+console.log(place);
