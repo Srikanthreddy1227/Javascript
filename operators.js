2e51dddc67267979437b5a15bdc2a03e584dcda6
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-04 11:47:05 +0530

    add opertors.js

---
+++ operators.js
@@ -0,0 +1,29 @@
+let sum= 2+3;
+console.log(sum); // arithmetic operators
+
+let sub= 5-2;
+console.log(sub); // arithmetic operators
+
+let mul= 5*2;
+console.log(mul); // arithmetic operators
+
+let div=10/2;
+console.log(div); // arithmetic operators
+
+let modulo=10%3;
+console.log(modulo); // arithmetic operators
+
+let a=10;
+a++;
+console.log(a); // increment operator
+
+let b=5;
+++b;
+console.log(b); // increment operator
+
+let c=5;
+--c;
+console.log(c);
+
+c--;
+console.log(c); // decrement operator
