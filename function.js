91beb00e1a7d7dee47fef6dd5791a25ec79bc1f5
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-04 11:21:37 +0530

    add function.js

---
+++ function.js
@@ -0,0 +1,22 @@
+// non primitive data types
+let marks =[34, 45, 56, 67, 78];
+console.log(marks[1]);
+
+let mixed = [45, "harry", 67.8, null];
+console.log(mixed);
+
+// objects in js
+
+let personditais={
+    name:"srikanth",
+    age:28,
+    education:"mca"
+};
+console.log(personditais.education);
+
+function name() {
+    console.log("well come to non primitive data types");
+    
+    
+}
+name();
