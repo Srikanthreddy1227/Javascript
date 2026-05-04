27a0d238d8c92dd2525eadc70837ab4b7606641a
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-04 12:21:41 +0530

    add condins.js

---
+++ conditions.js
@@ -0,0 +1,14 @@
+let age=8;
+let nationality="indian";
+
+if(nationality==="indian"){
+    console.log('Your are eligible for nationlity');
+    if(age>=18){
+        console.log('Your are eligible for vote');
+        
+    }
+    else{
+        console.log("you are not eligible for vote");
+        
+    }
+}
