29ac24bf4993712d149e0dea08a7d2baf23460c5
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-04 14:27:01 +0530

    add strings.js

---
+++ strings.js
@@ -0,0 +1,20 @@
+
+// let word1='hello ';
+// let word2="world";
+// console.log(word1+word2);
+// console.log();
+
+// let name='srikanth';
+// console.log(name.length);
+
+// let pass="1234568";
+// if(pass.length>=8){
+//     console.log("strong password");
+// }else{
+//     console.log("weak password");
+// }
+// let name="prajwal reddy";
+// console.log(name.charAt(0));
+// console.log(name.charAt(6));
+
+
