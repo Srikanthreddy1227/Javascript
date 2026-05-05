aea44f0e035790b6052eeed7ed6d29a3a5327831
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-05 16:15:56 +0530

    add demo.js

---
+++ demo.js
@@ -0,0 +1,29 @@
+// let a=10;
+// let b=20;
+
+// function mul(){
+//     return a*b;
+    
+    
+// }
+// console.log(mul());
+
+// let add=function(){
+//     let a=5;
+//     let b=10;
+//     return a*b;
+// }
+// console.log(add());
+
+// // arrow functions 
+// let square=(a)=>(a*a);
+// console.log(square(5));
+
+// let add=(a,b,c)=>(a+b+c);
+// console.log(add(5,5,4));
+
+// function add(a,b,c){
+//     return a+b-c;
+// }
+// console.log(add(5,5,1));
+
