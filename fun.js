94bbbd90f9c08d0b3076c2ed373705ad89fe3f34
Srikanth1227 <srikanthreddyb75@gmail.com>
2026-05-05 13:54:56 +0530

    add fun.js file

---
+++ fun.js
@@ -0,0 +1,23 @@
+//  Functions in javascript 
+
+// regular function
+function add() {
+    let a=10;
+    let b=20;
+    console.log(a+b);;
+    
+    
+}
+add();
+
+// function expression
+let sub = function() {
+    let a=10;
+    let b=20;
+    console.log(a-b);}
+sub();
+
+
+// arrow function
+let mul=()=>{let a=10;let b=20;console.log(a*b);}
+mul();
